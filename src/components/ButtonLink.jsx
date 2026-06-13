356bed87dd025a3b54becd7ed267eb8c1b85172c
William Liu <liutk.william@gmail.com>
2026-06-13 22:24:38 +1000

    minor update

--- src/components/ButtonLink.jsx
+++ src/components/ButtonLink.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function ButtonLink({ href, content }) {
+    const className = "bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-6 rounded shadow transition"
+    const isExternal = /^https?:\/\//.test(href)
+
+    if (isExternal) {
+        return (
+            <a
+                href={href}
+                className={className}
+                target="_blank"
+                rel="noreferrer"
+            >
+                {content}
+            </a>
+        )
+    }
+
     return (
-        <a
-            href={href}
-            className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-6 rounded shadow transition"
+        <Link
+            to={href}
+            className={className}
         >
             {content}
-        </a>
+        </Link>
     )
 }
 
